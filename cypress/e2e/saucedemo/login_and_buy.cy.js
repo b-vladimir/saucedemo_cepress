1e37badc13a95315aa57c0a71db7cad4ffd763f6
Volodymyr Borysov <>
2022-07-04 17:25:46 +0300

    add page asserts

--- cypress/e2e/saucedemo/login_and_buy.cy.js
+++ cypress/e2e/saucedemo/login_and_buy.cy.js
@@ -17,6 +17,7 @@ describe('Happy path', () => {
         page.login.typeUserNane(Cypress.env('username'))
         page.login.typePassword(Cypress.env('password'))
         page.login.clickLoginButton()
+        page.home.isPageOpen()
         // add to the cart a bacpack and a red t-shirt
         page.home.clickAddBackpack()
         page.home.isRemoveBackpackButtonDisplayed()
@@ -24,15 +25,19 @@ describe('Happy path', () => {
         page.home.isRemoveRedTShirtButtonDisplayed()
         // go to the cart
         page.home.clickCartButton()
+        page.cart.isPageOpen()
         // checkout
         page.cart.clickCheckoutButton()
+        page.yourInformation.isPageOpen()
         // your information
         page.yourInformation.typeFirstName(client.firstName)
         page.yourInformation.typeLastName(client.lastName)
         page.yourInformation.typePostalCode(client.postalCode)
         page.yourInformation.clickContinueButton()
+        page.checkout.isPageOpen()
         // finish checkout
         page.checkout.clickFinishButton()
+        page.checkoutComplete.isPageOpen()
         //assertions
         page.checkoutComplete.isPageOpen()
         page.checkoutComplete.getThankYouText().should('be.visible')
